5a8fe86a66debf549543143457b263ff58592548
Carlos Hernandez <honchkrow1995@gmail.com>
2025-08-05 04:27:53 -0600

    feat: Integrate the auth middleware to other endpoints

--- src/routes/event.routes.ts
+++ src/routes/event.routes.ts
@@ -2,12 +2,13 @@ import { Router } from "express";
 import { postTrackEvent, postTrackExternalEvent } from "../controllers/event.controller";
 import { validateEvent } from "../middlewares/eventValidation.middleware";
 import { eventArraySchema, externalEventArraySchema } from "../schemas/event.schema"; 
+import { authenticateToken } from "../middlewares/auth.middleware";
 
 
 const router = Router();
 
 // -- Event routes --
-router.post("/", validateEvent(eventArraySchema), postTrackEvent);
-router.post("/external", validateEvent(externalEventArraySchema), postTrackExternalEvent);
+router.post("/", authenticateToken, validateEvent(eventArraySchema), postTrackEvent);
+router.post("/external", authenticateToken, validateEvent(externalEventArraySchema), postTrackExternalEvent);
 
 export default router;
--- src/routes/stats.routes.ts
+++ src/routes/stats.routes.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { getPageViewsStats } from "../controllers/stats.controller";
 import { validateStatsQuery } from "../middlewares/statsQueryParams.middleware";
+import { authenticateToken } from "../middlewares/auth.middleware";
 
 const router = express.Router();
 
-router.get("/page-views", validateStatsQuery, getPageViewsStats);
+router.get("/page-views", authenticateToken, validateStatsQuery, getPageViewsStats);
 // router.get("/activity", validateActivityQuery, getActivityStats);
 
 export default router;
